ccdb80088b1eac270c71eab8e7d224f757f52852
Giole13 <119779618+Giole13@users.noreply.github.com>
2023-01-02 17:51:55 +0900

    [Jaewoo]Poker

---
+++ PokerGame/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace PokerGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TrumpCard trumpCard = new TrumpCard();
+            trumpCard.SetupTrumpCards();
+
+
+            int playerPoint = 10000;
+
+            int playerInput = 0;
+
+
+
+
+
+            while (true)
+            {
+                Console.WriteLine("※***********************※");
+                Console.WriteLine("※     POKER GAME    ※");
+                Console.WriteLine("***********************※");
+                Console.WriteLine("※Tab To Start※");
+                string tabInput = string.Empty;
+                tabInput = Console.ReadLine();
+
+
+                Console.Clear();
+
+                Console.WriteLine("※    컴퓨터의 카드 드로우   ※");
+
+
+                trumpCard.ShuffleCards();
+
+                trumpCard.ComputerCard(0); //컴퓨터 5장
+
+                Console.WriteLine("플레이어의 카드 드로우 차례\n내 카드를 확인 할려면 아무키나 입력하시오.");
+                tabInput = Console.ReadLine();
+
+
+                trumpCard.ComputerCard(5);   //플레이어 5장
+
+
+
+                Console.Write("\n배팅 금을 입력하시오: 패쓰는 0 을 입력하시오.");
+                Console.WriteLine("현재 소지 배팅금 {0} ", playerPoint);
+                playerInput = Convert.ToInt32(Console.ReadLine());
+                if (playerInput == 0)
+                {
+
+                }
+                else
+                {
+
+                    Console.WriteLine("컴퓨터가 두 장을 뽑습니다.");
+                    trumpCard.TwoCard(10);
+
+                    Console.WriteLine("카드 두장을 교체 하시겠습니까?: ");
+                    int.TryParse(Console.ReadLine(), out playerInput);
+
+
+                    //int usercard = trumpCard.ComputerCard(2);
+                    //if ((comOneCard < usercard && usercard < userCard) || (userCard < usercard && usercard < comOneCard))
+                    //{
+                    //    Console.WriteLine("배팅에서 승리 하셨습니다.");
+                    //    Console.WriteLine("배팅 승리 수당 {0} ", playerInput * 2);
+                    //    playerPoint += playerInput * 2;
+                    //}
+                    //else
+                    //{
+                    //    Console.WriteLine("배팅에서 패배 하셨습니다.");
+                    //    Console.WriteLine("잃은 수당 {0} ", playerInput);
+                    //    Console.WriteLine("☆★☆★☆★☆★☆★☆★☆★☆★");
+
+                    //    playerPoint -= playerInput;
+
+                    //}
+
+                }
+                if (playerPoint >= 100000)
+                {
+                   
+                    Console.WriteLine("\tYOU WIN!");
+                   
+                    break;
+                }
+                else if (playerPoint == 0)
+                {
+                    
+                    Console.WriteLine("최소 배팅금이 없습니다. 패배!");
+                    
+                    break;
+                }
+
+            }
+        }
+
+
+
+    }
+}
+
---
+++ PokerGame/Trump1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokerGame
+{
+
+    public class TrumpCard
+    {
+        private int[] trumpCardSet;      //내가 사용할 카드 세트
+        private string[] trumpCardMark;  // 트러픔 카드의 마크
+
+        public void SetupTrumpCards()
+        {
+            trumpCardSet = new int[52];
+            for (int i = 0; i < trumpCardSet.Length; i++)
+            {
+                trumpCardSet[i] = i + 1;
+
+            }   //loop: 카드를 셋업하는 루프
+                //트러픔 카드 마크 셋업
+            trumpCardMark = new string[4] { "♣", "♥", "◆", "♠" };
+        }       //SetupTrumpCards()
+
+        //!카드를 섞는 함수
+        public void ShuffleCards()
+        {
+            ShuffleCards(200);
+        }
+        //! 한장의 카드를 뽑아서 보여주는 함수
+
+        //!셔플 하고서 카드를 한 장 뽑아서 출력하는 함수
+        public void DrawCard()
+        {
+            ShuffleCards();
+            ComputerCard(200);
+        }   //ReRollCard()
+
+        public void ComputerCard(int comValue)      //5개 뽑는식 
+        {
+            int[] cardArray = new int[comValue + 5];
+            //int[] cardNumberArray = new int[comValue + 5];
+            string[] cardMark = new string[comValue + 5];
+            string[] cardNumber = new string[comValue + 5];
+            //카드 숫자와 카드 문양을 배열에 담는데 
+
+            int cardValue = 0;
+
+            for (int i = comValue; i < comValue + 5; i++)
+            {
+                cardArray[i] = trumpCardSet[i];
+
+                cardMark[i] = trumpCardMark[(cardArray[i] - 1) / 13];
+                cardNumber[i] = Math.Ceiling(cardArray[i] % 13.1).ToString();
+
+                //cardValue = (int)Math.Ceiling(card % 13.1);
+                //comValue++;
+
+
+
+                switch (cardNumber[i])
+                {
+                    case "11":
+                        cardNumber[i] = "J";
+                        break;
+                    case "12":
+                        cardNumber[i] = "Q";
+                        break;
+                    case "13":
+                        cardNumber[i] = "K";
+                        break;
+
+                }
+
+            }
+
+            Console.WriteLine("뽑은 카드는 {0} {1} {2} {3} {4} {5} {6} {7} {8} {9}입니다."
+                , cardMark[comValue +0], cardNumber[comValue + 0], cardMark[comValue + 1], cardNumber[comValue + 1], cardMark[comValue + 2], 
+                cardNumber[comValue + 2], cardMark[comValue + 3], cardNumber[comValue + 3], cardMark[comValue + 4], cardNumber[comValue + 4]);
+            Console.WriteLine(" -----  -----  -----  -----  ----- ");
+            Console.WriteLine("|{0} {1}| |{2} {3}| |{4} {5}| |{6} {7}| |{8} {9}|",
+                cardMark[comValue +0], cardNumber[comValue +0], cardMark[comValue + 1], cardNumber[comValue + 1], 
+                cardMark[comValue + 2], cardNumber[comValue + 2], cardMark[comValue + 3], cardNumber[comValue + 3],
+                cardMark[comValue + 4], cardNumber[comValue + 4]);
+            Console.WriteLine("|    | |    | |    | |    | |    | ");
+            Console.WriteLine("|{0} {1}| |{2} {3}| |{4} {5}| |{6} {7}| |{8} {9}|",
+                cardMark[comValue+0], cardNumber[comValue+0], cardMark[comValue + 1], cardNumber[comValue + 1],
+                cardMark[comValue + 2], cardNumber[comValue + 2], cardMark[comValue + 3], cardNumber[comValue + 3],
+                cardMark[comValue + 4], cardNumber[comValue + 4]);
+            Console.WriteLine(" -----  -----  -----  -----  ----- ");
+
+        }   //RollCard
+
+        
+
+
+        public void TwoCard(int comValue)
+        {
+            int[] cardArray = new int[comValue + 2];
+            //int[] cardNumberArray = new int[comValue + 5];
+            string[] cardMark = new string[comValue + 2];
+            string[] cardNumber = new string[comValue + 2];
+            //카드 숫자와 카드 문양을 배열에 담는데 
+
+            int cardValue = 0;
+
+            for (int i = comValue; i < comValue + 2; i++)
+            {
+                cardArray[i] = trumpCardSet[i];
+
+                cardMark[i] = trumpCardMark[(cardArray[i] - 1) / 13];
+                cardNumber[i] = Math.Ceiling(cardArray[i] % 13.1).ToString();
+
+                //cardValue = (int)Math.Ceiling(card % 13.1);
+                //comValue++;
+
+
+
+                switch (cardNumber[i])
+                {
+                    case "11":
+                        cardNumber[i] = "J";
+                        break;
+                    case "12":
+                        cardNumber[i] = "Q";
+                        break;
+                    case "13":
+                        cardNumber[i] = "K";
+                        break;
+
+                }
+
+            }
+
+            Console.WriteLine("뽑은 카드는 {0} {1} {2} {3}입니다."
+                , cardMark[comValue + 0], cardNumber[comValue + 0], cardMark[comValue + 1], cardNumber[comValue + 1]);
+            Console.WriteLine(" -----  ----- ");
+            Console.WriteLine("|{0} {1}\t|{2} {3}| ",
+                cardMark[comValue + 0], cardNumber[comValue + 0], cardMark[comValue + 1], cardNumber[comValue + 1]);
+            Console.WriteLine("|    |\t|    |  ");
+            Console.WriteLine("|{0} {1}|\t|{2} {3}|",
+                cardMark[comValue + 0], cardNumber[comValue + 0], cardMark[comValue + 1], cardNumber[comValue + 1]);
+            Console.WriteLine(" -----  ----- ");
+        }   //RollCard
+
+
+        public void PrintCardSet()
+        {
+            foreach (int card in trumpCardSet)
+            {
+                Console.Write("{0} ", card);
+            }   //PrintCardSet()
+        }
+
+        //!내부에서 사용하는 배열을 1번 섞는 함수.
+        public int[] ShuffleOnce(int[] intArray)
+        {
+            Random random = new Random();
+            int sourIndex = random.Next(0, intArray.Length);
+            int destIndex = random.Next(0, intArray.Length);
+
+            int tempVarible = intArray[sourIndex];
+            intArray[sourIndex] = intArray[destIndex];
+            intArray[destIndex] = tempVarible;
+
+            return intArray;
+        }
+
+        private void ShuffleCards(int howManyLoop)
+        {
+            for (int i = 0; i < howManyLoop; i++)
+            {
+                trumpCardSet = ShuffleOnce(trumpCardSet);
+            } //ShuffleCard//
+        }
+    }
+}
+
